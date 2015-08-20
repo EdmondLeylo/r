3c6298ad5b3bb24767e9042492acf97d94df3490
Edmond Leylo <edmondlaylo@gmail.com>
2015-08-20 14:27:09 +0300

    2

--- R191/UpgradeDB.bat
+++ R191/UpgradeDB.bat
@@ -3,7 +3,7 @@
 IF "%~1"=="" (
 :::::::::::::::::::::::::::::::
 SET ServerName=EDMOND-PC\edmond
-SET DBName=OffersII
+SET DBName=OffersI
 SET FileName=R191.sql
 ::If not Integrated Security set the username and password
 SET Username=sa
